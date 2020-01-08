ca4b547d3c136b6753a1ab1f5d0eaf200136b9b0
minkyeong417 <55428050+minkyeong417@users.noreply.github.com>
2020-01-08 20:10:36 +0900

    frequency

--- tuple.py
+++ tuple.py
@@ -1,11 +1,13 @@
 filename = input('Enter a file name: ')
 handle = open(filename, 'r')
 dic = dict()
-for line in handle:
-    if line.startswith('From '):
-        words = line.split()
-        time = words[5] #time == '09:14:16'
-        dic[time[:2]] = dic.get(time[:2],0) + 1
-
-for hr,ct in sorted(dic.items()):
-    print(hr, ct)
+text = handle.read()
+for word in list(text):
+    if 'a'< word <'z' or 'A'< word <'Z':
+        dic[word] = dic.get(word,0) + 1
+lists = dic.items()
+newl = list()
+for k,v in lists:
+    newl.append((v,k))
+for a,b in sorted(newl,reverse = True):
+    print(b)
